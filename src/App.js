dc0409eab11a7733f193f52add626bdc433b5d56
carolineRe13 <caroline112010@hotmail.de>
2022-04-01 23:37:57 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+import React, { Component } from 'react'
+ 
+import AudioReactRecorder, { RecordState } from 'audio-react-recorder'
+ 
+class App extends Component {
+  constructor(props) {
+    super(props)
+ 
+    this.state = {
+      recordState: null
+    }
+  }
+ 
+  start = () => {
+    this.setState({
+      recordState: RecordState.START
+    })
+  }
+ 
+  stop = () => {
+    this.setState({
+      recordState: RecordState.STOP
+    })
+  }
+ 
+  //audioData contains blob and blobUrl
+  onStop = (audioData) => {
+    console.log('audioData', audioData)
+
+    // TODO call api
+  }
+ 
+  render() {
+    const { recordState } = this.state
+ 
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AudioReactRecorder state={recordState} onStop={this.onStop} />
+ 
+        <button onClick={this.start}>Start</button>
+        <button onClick={this.stop}>Stop</button>
       </header>
     </div>
-  );
+    )
+  }
 }
 
-export default App;
+export default App
+
